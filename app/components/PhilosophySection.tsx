54ed0ff4e5cb77cbb703fd7deda2669a3eefa927
Naufal Rizqi Ilham Gibran <ilhamg18rn@gmail.com>
2026-09-24 14:00:15 +0700

    [Naufal] update lagi

--- app/components/PhilosophySection.tsx
+++ app/components/PhilosophySection.tsx
@@ -8,7 +8,7 @@ export default function PhilosophySection() {
     <section className="w-full bg-[#F4F4F6]/70 dark:bg-[#161618] border-y border-neutral-200/80 dark:border-neutral-800 py-14 sm:py-20 px-4 sm:px-6 my-8 sm:my-12 transition-colors duration-300">
       <div className="max-w-3xl mx-auto text-center space-y-4 sm:space-y-6">
         {/* Enso Small Art */}
-        <ScrollReveal direction="down" delay={0.1}>
+        {/* <ScrollReveal direction="down" delay={0.1}>
           <div className="flex justify-center">
             <Image
               src="/asset/tile-03-enso-small.png"
@@ -18,7 +18,7 @@ export default function PhilosophySection() {
               className="opacity-75 dark-invert-asset w-10 sm:w-12 h-10 sm:h-12"
             />
           </div>
-        </ScrollReveal>
+        </ScrollReveal> */}
 
         {/* Philosophy Quote */}
         <ScrollReveal direction="up" delay={0.25} duration={0.8}>
